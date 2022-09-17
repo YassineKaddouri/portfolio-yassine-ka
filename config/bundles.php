a6fbf4ca80674c827e7fe42f54fda0b42d9997a2
YassineKaddouri <yassinebts93@gmail.com>
2022-09-17 23:49:15 +0100

    migrate

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
